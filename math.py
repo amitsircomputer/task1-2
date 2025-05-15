517af7a32f2ae498c58e0fc186f6e6b98d084065
amitsircomputer <amitsircomputer@gmail.com>
2025-05-15 21:35:59 +0530

    Add files via upload

--- math.py
+++ math.py
@@ -1,7 +1,13 @@
-a=int(input('Enter the First Number: '))
+'''a=int(input('Enter the First Number: '))
 b=int(input('Enter the Second Number: '))
 
 print('Addition: ',(a+b))
 print('Subtraction: ',(a-b))
 print('Multiplication: ',(a*b))
 print('Division: ', (a/b))
+'''
+
+fn=input('Enter First Name: ')
+ln=input('Enter Last Name: ')
+
+print('Hello '+ fn + ' ' + ln + '! Welcome to the Python Program ')
